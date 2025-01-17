972909032cad4a6671480091b88a0ea2e84bde9b
dimitry-lzs <lazanasdimitrios@gmail.com>
2025-01-17 09:44:08 +0200

    make it link

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Header.less';
+import { Link } from 'react-router-dom';
 
 export default function Header({ title }: { title: string }) {
     return (
         <div className='Header'>
             <div className='PageTitle'>
-                <div className='Logo' />
+                <Link to='/main'><div className='Logo' /></Link>
                 <div className='Title'>{title}</div>
             </div>
         </div>
