6696c0fdc92c8de1712bc996c72ebd515cb24bf6
dimitry-lzs <lazanasdimitrios@gmail.com>
2025-01-18 05:12:03 +0200

    add radio component

---
+++ src/components/Radio/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './Radio.less';
+
+export default function Radio({ options = [], selectedValue = '', onChange = () => {} }: {
+    options?: { value: string; label: string }[];
+    selectedValue?: string;
+    onChange?: (value: string) => void;
+}) {
+    return (
+        <div className='radioContainer'>
+            {options.map(({ value, label }) => (
+                <label key={value} className='radioLabel'>
+                    <input type='radio' name='radio' value={value} checked={selectedValue === value} onChange={() => onChange(value)} />
+                    <span className='checkmark'></span>
+                    {label}
+                </label>
+            ))}
+        </div>
+    );
+}
