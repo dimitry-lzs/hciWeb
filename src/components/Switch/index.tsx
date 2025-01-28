d7fa9b00e9fb88eef8ba2d51ee91bbfe6e5dc9d2
dimitry-lzs <lazanasdimitrios@gmail.com>
2025-01-28 15:29:05 +0200

    change energy

--- src/components/Switch/index.tsx
+++ src/components/Switch/index.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+
 import './Switch.less';
 
-export default function Switch() {
+export default function Switch({
+    on,
+    setOnOff,
+    onIcon,
+    offIcon,
+    onColor,
+    offColor
+}: {
+    on: boolean;
+    setOnOff: (on: boolean) => void;
+    onIcon?: JSX.Element;
+    offIcon?: JSX.Element;
+    onColor?: string;
+    offColor?: string;
+}) {
     return (
-        <div className="switchComponent">
-            <label className="switch">
-                <input type="checkbox" />
-                <span className="slider round"></span>
+        <div className='switchComponent'>
+            <label className='switch'>
+                <input type='checkbox' checked={on} onChange={checked => {
+                    setOnOff(checked.target.checked);
+                }} />
+                <span className='slider round' {...(on ? { style: { backgroundColor: onColor } } : { style: { backgroundColor: offColor } })}>
+                    <span className='switchIcon'>{on ? <>{onIcon}</> : <>{offIcon}</>}</span>
+                </span>
             </label>
         </div>
     );
--- src/pages/SpecialOccasions/index.tsx
+++ src/pages/SpecialOccasions/index.tsx
@@ -19,6 +19,22 @@ const radioOptions = [
     { value: 'hamBlue', label: 'Hamlindigo Blue' }
 ];
 
+const presets = [
+    { value: 'party', label: 'Party' },
+    { value: 'concert', label: 'Concert' },
+    { value: 'sport', label: 'Sport event' }
+];
+const lightPatern = [
+    { value: 'random', label: 'Random' },
+    { value: 'slow', label: 'Slow' },
+    { value: 'fast', label: 'Fast' }
+];
+const colorPattern = [
+    { value: 'light', label: 'Light' },
+    { value: 'dark', label: 'Dark' },
+    { value: 'rainbow', label: 'Rainbow' }
+];
+
 function BulbColors({ selectedColor, setSelectedColor }: {
     selectedColor: string;
     setSelectedColor: (color: string) => void;
@@ -28,25 +44,14 @@ function BulbColors({ selectedColor, setSelectedColor }: {
 
 export default function SpecialOccasions() {
     const navigate = useNavigate();
-    const presets = [
-        { value: 'party', label: 'Party' },
-        { value: 'concert', label: 'Concert' },
-        { value: 'sport', label: 'Sport event' }
-    ];
-    const lightPatern = [
-        { value: 'random', label: 'Random' },
-        { value: 'slow', label: 'Slow' },
-        { value: 'fast', label: 'Fast' }
-    ];
-    const colorPattern = [
-        { value: 'light', label: 'Light' },
-        { value: 'dark', label: 'Dark' },
-        { value: 'rainbow', label: 'Rainbow' }
-    ];
 
     const [color1, setColor1] = useState('blue');
     const [color2, setColor2] = useState('green');
     const [color3, setColor3] = useState('purple');
+
+    const [selectedPreset, setSelectedPreset] = useState('party');
+    const [selectedLightPattern, setSelectedLightPattern] = useState('random');
+    const [selectedColorPattern, setSelectedColorPattern] = useState('light');
 
     return (
         <div className='SpecialOccasions'>
@@ -55,7 +60,8 @@ export default function SpecialOccasions() {
                 <div className='Controls'>
                     <div className='MenuRow'>
                         <div className='SelectMenu'>
-                            <div className='Label'>Presets:</div><Select options={presets} />
+                            <div className='Label'>Presets:</div>
+                            <Select options={presets} onSelect={setSelectedPreset} />
                         </div>
                         <LightBulb color={color1} />
                         <div className='RadioOptions'>
@@ -64,7 +70,8 @@ export default function SpecialOccasions() {
                     </div>
                     <div className='MenuRow'>
                         <div className='SelectMenu'>
-                            <div className='Label'>Light Play Pattern:</div><Select options={lightPatern} />
+                            <div className='Label'>Light Play Pattern:</div>
+                            <Select options={lightPatern} onSelect={setSelectedLightPattern} />
                         </div>
                         <LightBulb color={color2} />
                         <div className='RadioOptions'>
@@ -73,7 +80,8 @@ export default function SpecialOccasions() {
                     </div>
                     <div className='MenuRow'>
                         <div className='SelectMenu'>
-                            <div className='Label'>Color Switching Pattern:</div><Select options={colorPattern} />
+                            <div className='Label'>Color Switching Pattern:</div>
+                            <Select options={colorPattern} onSelect={setSelectedColorPattern} />
                         </div>
                         <LightBulb color={color3} />
                         <div className='RadioOptions'>
